4a23d392864cc8a66aac0d6371d58ea5b4cf90d8
Fabian Keller <github.100.fkeller@spamgourmet.com>
2022-05-26 10:04:05 +0200

    modelled question component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import { Button, Space } from "antd";
 import { Card } from "antd";
+import { useState } from "react";
 import { ButtonGreen, ButtonRed, Center } from "./helper_components";
+import { Question } from "./Question";
 
 function App() {
-  return (
-    <div style={{ maxWidth: 800, margin: "0 auto" }}>
-      <Center style={{ marginTop: 30 }}>
-        <Card style={{ width: 300, height: 300 }}></Card>
-      </Center>
-      <Center style={{ marginTop: 30 }}>
-        <Space>
-          <ButtonRed>Wrong</ButtonRed>
-          <ButtonGreen>Correct</ButtonGreen>
-        </Space>
-      </Center>
-      <Center style={{ marginTop: 30 }}>
-        <Button type="primary">Got it</Button>
-      </Center>
-    </div>
-  );
+  return <Question question="10 + 20" />;
 }
 
 export default App;
