ebb4a0fa989d77da9175fc38bb386664c246d0f3
Fabian Keller <github.100.fkeller@spamgourmet.com>
2022-05-26 09:40:26 +0200

    first draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from "react";
-import "./App.css";
+import { Button, Space } from "antd";
+import { Card } from "antd";
+import { ButtonGreen, ButtonRed, Center } from "./helper_components";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div style={{ maxWidth: 800, margin: "0 auto" }}>
+      <Center style={{ marginTop: 30 }}>
+        <Card style={{ width: 300, height: 300 }}></Card>
+      </Center>
+      <Center style={{ marginTop: 30 }}>
+        <Space>
+          <ButtonRed>Wrong</ButtonRed>
+          <ButtonGreen>Correct</ButtonGreen>
+        </Space>
+      </Center>
+      <Center style={{ marginTop: 30 }}>
+        <Button type="primary">Got it</Button>
+      </Center>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/helper_components.tsx
@@ -0,0 +1,53 @@
+import styled from "@emotion/styled";
+import { Button } from "antd";
+
+type GenericProps = {
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+};
+
+export function Center({ children, style }: GenericProps) {
+  return (
+    <div style={{ ...style, display: "flex", justifyContent: "center" }}>
+      {children}
+    </div>
+  );
+}
+
+// https://stackoverflow.com/questions/55717479/how-to-pass-props-to-a-styled-component-in-emotion-using-typescript
+const ButtonWrapper = styled.div<{ color: string }>`
+  .ant-btn-primary {
+    background-color: ${(props) => props.color};
+    border-color: ${(props) => props.color};
+  }
+`;
+
+// Antd colors:
+// https://ant.design/docs/spec/colors
+
+const RED = "#f5222d";
+const GREEN = "#52c41a";
+
+// Note that modifying the color of Antd Buttons is a bit awkward, because it needs to set
+// two colors internally, and modifying the class requires a wrapper div:
+// https://stackoverflow.com/questions/56718231/is-there-a-way-to-change-antd-internal-buttons-color
+
+export function ButtonRed({ children, style }: GenericProps) {
+  return (
+    <ButtonWrapper color={RED}>
+      <Button type="primary" style={{ ...style }}>
+        {children}
+      </Button>
+    </ButtonWrapper>
+  );
+}
+
+export function ButtonGreen({ children, style }: GenericProps) {
+  return (
+    <ButtonWrapper color={GREEN}>
+      <Button type="primary" style={{ ...style }}>
+        {children}
+      </Button>
+    </ButtonWrapper>
+  );
+}
